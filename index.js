4006720515dcc8a1b34f7cb997183f0cbbf9926f
downhillskater91 <varuns.nandyal@gmail.com>
2021-01-28 22:54:06 -0600

    Added new endpoints

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const football = require('./routes/football');
+const espn = require('./routes/espn');
 
 const app = express();
 
 // set cors
 app.use(cors({
-  origin: ['https://vsnandy.github.io', 'http://localhost:3000', 'http://localhost:8000']
+  origin: ['https://vsnandy.github.io', 'http://localhost:3000', 'http://localhost:8000', 'http://localhost:8001'],
+  credentials: true,
+  exposedHeaders: ["set-cookie"]
 }));
 
 dotenv.config();
@@ -18,6 +21,7 @@ const port = process.env.PORT || 8000;
 
 // setup api endpoints for use by app server
 app.use('/football', football.router);
+app.use('/espn', espn.router);
 
 app.listen(port, () => {
   console.log('App is running on http://localhost:' + port);
