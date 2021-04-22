27abf4120bde5478160adb4c4d66f7451b1e3d28
vsnandy <varuns.nandyal@gmail.com>
2021-04-22 11:16:54 -0500

    Proxy addition

--- services/nba/scripts/nba.py
+++ services/nba/scripts/nba.py
@@ -3,6 +3,10 @@ import sys
 from nba_api.stats.endpoints import commonallplayers, leagueleaders
 from nba_api.stats.static import teams
 import json
+import requests
+from lxml.html import fromstring
+import random
+import time
 
 custom_headers = {
     'Host': 'stats.nba.com',
@@ -16,9 +20,32 @@ custom_headers = {
     'x-nba-stats-token': 'true'
 }
 
+proxies = ['195.154.84.106:5566', '104.236.195.241:8080', '176.28.64.225:3128', 
+    '138.36.151.15:8090', '80.244.230.86:8080', '46.175.70.69:44239', '139.59.254.67:8080', 
+    '103.154.241.252:8080', '54.255.218.28:3128', '203.26.189.28:3128', '68.183.107.45:8080', 
+    '128.199.188.60:8080', '128.199.60.240:8080', '183.88.226.50:8080', '145.40.68.155:80', 
+    '178.33.148.10:3128', '103.213.228.62:3128', '139.59.252.150:8080', '168.119.103.160:10303', 
+    '65.0.113.10:3128', '178.62.207.37:8080', '178.128.59.125:8118', '1.2.187.126:44617', 
+    '103.28.121.58:80', '85.185.159.75:8080', '157.90.4.18:8001', '132.248.196.2:8080'
+]
+
 ###################
 ## Helper Methods #
 ###################
+
+'''
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr'):
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+
+    return list(proxies)
+'''
 
 # Get Team ID from full name
 def get_team_id(team_name):
@@ -28,18 +55,20 @@ def get_team_id(team_name):
                 return team['id']
         return -1
     except Exception as e:
-        return "Error"
+        return -2
 
 # Get Player ID from full name
 def get_player_id(player_name):
     try:
-        all_players = commonallplayers.CommonAllPlayers(headers=custom_headers, timeout=100)
+        time.sleep(1)
+        all_players = commonallplayers.CommonAllPlayers(headers=custom_headers, 
+            proxy=proxies[0], timeout=10)
         for player in all_players.get_dict()['resultSets'][0]['rowSet']:
             if player[2].lower() == player_name.lower():
                 return player[0]
         return -1
     except Exception as e:
-        return "Error"
+        return -2
 
 ###############
 ## Functions ##
@@ -55,12 +84,16 @@ def get_player_details(args):
 # Get league leaders for specified stat category
 def get_league_leaders(args):
     try:
-        leaders = leagueleaders.LeagueLeaders(headers=custom_headers, stat_category_abbreviation=args[0], timeout=100)
+        time.sleep(1)
+        leaders = leagueleaders.LeagueLeaders(headers=custom_headers, 
+            stat_category_abbreviation=args[0], proxy=proxies[0], timeout=10)
         print(leaders.get_json())
         sys.stdout.flush()
         return
     except Exception as e:
-        return "Error"
+        print(-2)
+        sys.stdout.flush()
+        return
 
 ###############
 ## Variables ##
